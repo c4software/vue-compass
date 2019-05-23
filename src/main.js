ea40b970a4cb9323e6f0f6cc284fb7050a384730
Valentin Brosseau <c4software@gmail.com>
2019-05-23 13:48:58 +0200

    Implement compass watch

--- src/main.js
+++ src/main.js
@@ -5,5 +5,26 @@ import { debug } from "./config.js";
 Vue.config.devtools = debug;
 
 new Vue({
-  el: "#app"
+  el: "#app",
+  data() {
+    return {
+      alpha: 0,
+      beta: 0,
+      gamma: 0
+    };
+  },
+  mounted() {
+    window.addEventListener("deviceorientation", evt => {
+        this.alpha = evt.alpha;
+        this.beta = evt.beta;
+        this.gamma = evt.gamma;
+      },
+      false
+    );
+  },
+  computed: {
+    angle() {
+      return `transform: rotate(${this.alpha || 45}deg)`;
+    }
+  }
 });
